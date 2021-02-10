aaab87f203e2b15ae680ba99bed7967b30e82a3d
Hüseyin Mustafa <huseyinbanna216@gmail.com>
2021-02-10 22:21:58 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+
+while True:
+    print("balık (1)" )
+    print("koç(2)" )
+    print("boğa (3)")
+    print("ikizler (4)" )
+    print("yengeç(5)")
+    print("aslan(6)")
+    print("başak(7)")
+    print("terazi(8)")
+    print("akrep (9)")
+    print("yay (10)")
+    print("kova (11)")
+    isim=input("isminiz.")
+    seçim=input("burcunuzu girin")
+
+    if seçim=="1":
+        print("sen kurnazsin",isim)
+        break
+
+    elif seçim=="2":
+        print("yüreğin büyük senin",isim)
+        break
+
+    elif seçim=="3":
+        print("sakın ol",isim)
+        break
+    elif seçim=="4":
+        print("neden sorgula",isim)
+        break
+    elif seçim=="5":
+        print("çok havalısın",isim)
+        break
+    elif seçim=="6":
+        print("güç sende",isim)
+        break
+    elif seçim=="7":
+        print("özledin diymi",isim)
+        break
+    elif seçim=="8":
+        print("sen varya yılansın",isim)
+        break
+    elif seçim=="9":
+        print("anlat sen dolusun",isim)
+        break
+    elif seçim=="10":
+        break
+        print("aklınsın ",isim)
+        break
+    elif seçim=="11":
+        print("yaratıcı ruhlu",isim)
+        break
+
+
